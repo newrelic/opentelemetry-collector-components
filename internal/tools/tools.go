769612a2abe8379e445c631f42e0a6f7af15668a
Roger Coll <rogercoll@protonmail.com>
2023-01-04 10:28:45 +0100

    feat: add internal tools package

---
+++ internal/tools/tools.go
@@ -0,0 +1,15 @@
+//go:build tools
+// +build tools
+
+package tools // import "github.com/cristianciutea/opentelemetry-components/internal/tools"
+import (
+	_ "github.com/client9/misspell/cmd/misspell"
+	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
+	_ "github.com/google/addlicense"
+	_ "github.com/jcchavezs/porto/cmd/porto"
+	_ "github.com/open-telemetry/opentelemetry-collector-contrib/cmd/mdatagen"
+	_ "github.com/pavius/impi/cmd/impi"
+	_ "go.opentelemetry.io/build-tools/checkdoc"
+	_ "go.opentelemetry.io/build-tools/crosslink"
+	_ "golang.org/x/tools/cmd/goimports"
+)
