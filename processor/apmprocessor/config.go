d320b892100e17ebf698159e402d4b1529488764
Julien Le Goff <jlegoff@newrelic.com>
2023-08-24 18:21:58 +0200

    chore: change config attribute name

--- processor/apmprocessor/config.go
+++ processor/apmprocessor/config.go
@@ -9,7 +9,7 @@ import (
 
 type Config struct {
 	// If set to true, will set the `instrumentation.provider` attribute to `newrelic-opentelemetry`
-	ChangeInstrumentationProvider bool `mapstructure:"changeInstrumentationProvider"`
+	ChangeInstrumentationProvider bool `mapstructure:"change_instrumentation_provider"`
 }
 
 var _ component.Config = (*Config)(nil)
