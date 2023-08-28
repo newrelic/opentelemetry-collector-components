6ee31fa3f0d4444a4438607e4111a9117a2e74f9
Julien Le Goff <jlegoff@newrelic.com>
2023-08-28 18:29:31 +0200

    fix: don't filter out the instrumentation.provider attribute

--- connector/apmconnector/attribute_filter.go
+++ connector/apmconnector/attribute_filter.go
@@ -12,7 +12,7 @@ type AttributeFilter struct {
 }
 
 func NewAttributeFilter() *AttributeFilter {
-	return &AttributeFilter{attributesToKeep: []string{"os.description", "telemetry.auto.version", "telemetry.sdk.language", "host.name",
+	return &AttributeFilter{attributesToKeep: []string{"instrumentation.provider", "os.description", "telemetry.auto.version", "telemetry.sdk.language", "host.name",
 		"os.type", "telemetry.sdk.name", "process.runtime.description", "process.runtime.version", "telemetry.sdk.version",
 		"host.arch", "service.name", "service.instance.id"}}
 }
