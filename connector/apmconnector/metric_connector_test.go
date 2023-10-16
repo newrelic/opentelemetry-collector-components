d13fd76680e3144a68f9f215b804d80d0344c18f
Alan West <3676547+alanwest@users.noreply.github.com>
2023-10-16 15:54:17 -0700

    Fix test

--- connector/apmconnector/metric_connector_test.go
+++ connector/apmconnector/metric_connector_test.go
@@ -32,15 +32,17 @@ func TestConvertOneSpanToMetrics(t *testing.T) {
 	logger, _ := zap.NewDevelopment()
 	config := Config{ApdexT: 0.5}
 	metrics := ConvertTraces(logger, &config, traces)
-	assert.Equal(t, 5, metrics.MetricCount())
+	assert.Equal(t, 2, metrics.MetricCount())
 	rm := metrics.ResourceMetrics().At(0)
 	serviceName, _ := rm.Resource().Attributes().Get("service.name")
 	assert.Equal(t, "service", serviceName.AsString())
 	sm := rm.ScopeMetrics().At(0)
-	checkSumMetric(t, "apm.service.transaction.apdex", 1, sm.Metrics())
-	checkSumMetric(t, "apm.service.apdex", 1, sm.Metrics())
+
+	// TODO: The commented out metrics will be generated when we generate them from spans for languages like Ruby
+	// checkSumMetric(t, "apm.service.transaction.apdex", 1, sm.Metrics())
+	// checkSumMetric(t, "apm.service.apdex", 1, sm.Metrics())
 	checkHistogramMetric(t, "apm.service.overview.web", 1, sm.Metrics())
-	checkHistogramMetric(t, "apm.service.transaction.duration", 1, sm.Metrics())
+	// checkHistogramMetric(t, "apm.service.transaction.duration", 1, sm.Metrics())
 	checkHistogramMetric(t, "apm.service.transaction.overview", 1, sm.Metrics())
 }
 
